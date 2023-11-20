0382c126e5f6b2bf8a5045582221c276414001c6
liyusang1 <liyusang1@naver.com>
2023-11-21 00:45:18 +0900

    [feat] SpringSecurity Jwt 관련 코드 추가
    JwtAuthenticationFilter
    JwtAuthorizationFilter
    JwtKey
    JwtProperties
    JwtProvider
    SigningKeyResolver

---
+++ src/main/java/com/example/yanolja/global/jwt/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.example.yanolja.global.jwt;
+
+/**
+ * JWT 기본 설정값
+ */
+public class JwtProperties {
+
+    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 10; // 10분 -> 600000
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 10;
+
+    public static final String COOKIE_NAME = "JWT-AUTHENTICATION";
+}
