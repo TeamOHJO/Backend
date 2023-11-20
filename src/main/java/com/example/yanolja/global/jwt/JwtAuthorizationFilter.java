c076e7023d6036462ce85ec736a07a417a68d261
liyusang1 <liyusang1@naver.com>
2023-11-21 00:45:18 +0900

    [feat] SpringSecurity Jwt 관련 코드 추가
    JwtAuthenticationFilter
    JwtAuthorizationFilter
    JwtKey
    JwtProperties
    JwtProvider
    SigningKeyResolver

---
+++ src/main/java/com/example/yanolja/global/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,98 @@
+package com.example.yanolja.global.jwt;
+
+import com.example.yanolja.domain.user.repository.UserRepository;
+import com.example.yanolja.global.springsecurity.PrincipalDetails;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * JWT를 이용한 인증
+ */
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthorizationFilter.class);
+
+    public JwtAuthorizationFilter(
+        UserRepository userRepository,
+        JwtProvider jwtProvider
+    ) {
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain chain
+    ) throws IOException, ServletException {
+        String accessToken = null;
+        try {
+            // cookie 에서 JWT token을 가져옵니다.
+            accessToken = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME)).findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
+        } catch (Exception ignored) {
+            //
+        }
+
+        if (accessToken != null) {
+            try {
+                //현재 토큰을 사용 하여 인증을 시도 합니다.
+                Authentication authentication = getUsernamePasswordAuthenticationToken(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (ExpiredJwtException ex) {
+                try {
+                    String userEmail = ex.getClaims().getSubject();
+                    String refreshedToken = jwtProvider.createRefreshToken(
+                        userEmail); // 이 메서드를 구현하여 토큰을 새로고침.
+                    Authentication authentication = getUsernamePasswordAuthenticationToken(
+                        refreshedToken); //refresh토큰 생성
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } catch (Exception e) {
+                    logger.error("Refresh token 생성 중 에러 발생", e);
+                }
+
+                //토큰이 만료된 경우 refresh토큰을 사용하여 새로운 jwt토큰을 생성
+                Cookie cookie = new Cookie(JwtProperties.COOKIE_NAME, null);
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * JWT 토큰으로 User를 찾아서 UsernamePasswordAuthenticationToken를 만들어서 반환한다. User가 없다면 null
+     */
+    private Authentication getUsernamePasswordAuthenticationToken(String token) {
+        String email = jwtProvider.getEmail(token);
+        if (email != null) {
+            return userRepository.findByEmail(email)
+                .map(PrincipalDetails::new)
+                .map(principalDetails -> new UsernamePasswordAuthenticationToken(
+                    principalDetails, // principal
+                    null, // credentials
+                    principalDetails.getAuthorities()
+                )).orElseThrow(IllegalAccessError::new);
+        }
+        return null; // 유저가 없으면 NULL
+    }
+}
