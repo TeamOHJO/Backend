9a0c6b6291c46ad8926624cca2b212a1064994f6
liyusang1 <liyusang1@naver.com>
2023-11-21 21:10:40 +0900

    [feat] signup에 phonenumberREGEX 추가
    signup에 phonenumberREGEX 추가

--- src/main/java/com/example/yanolja/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/yanolja/domain/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.yanolja.domain.user.dto.CreateUserRequest;
 import com.example.yanolja.domain.user.dto.CreateUserResponse;
 import com.example.yanolja.domain.user.error.EmailDuplicateError;
 import com.example.yanolja.domain.user.error.InvalidEmailException;
+import com.example.yanolja.domain.user.error.InvalidPhonenumberException;
 import com.example.yanolja.domain.user.repository.UserRepository;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +18,10 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private static final String EMAIL_PATTERN =
         "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private static final String PHONENUMBER_REGEX =
+        "^010[0-9]{8}$";
+    private static final Pattern emailPattern = Pattern.compile(EMAIL_PATTERN);
+    private static final Pattern phonenumberPattern = Pattern.compile(PHONENUMBER_REGEX);
 
     @Override
     public CreateUserResponse signup(CreateUserRequest createUserRequest) {
@@ -25,6 +29,11 @@ public class UserServiceImpl implements UserService {
         //이메일 유효성 검사
         if (!isValidEmail(createUserRequest.email())) {
             throw new InvalidEmailException();
+        }
+
+        //휴대폰 번호 유효성 검사
+        if (!isValidPhonenumber(createUserRequest.phonenumber())) {
+            throw new InvalidPhonenumberException();
         }
 
         //TODO ::이메일 인증
@@ -42,7 +51,12 @@ public class UserServiceImpl implements UserService {
     }
 
     private static boolean isValidEmail(String email) {
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = emailPattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private static boolean isValidPhonenumber(String phonenumber) {
+        Matcher matcher = phonenumberPattern.matcher(phonenumber);
         return matcher.matches();
     }
 }
