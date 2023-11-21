11d2281d3d650682fdc26cf7725c3bee89565ec6
liyusang1 <liyusang1@naver.com>
2023-11-21 21:12:36 +0900

    [refactor] UserControllerAdvice 코드 ResponseDTO에 맞게 refactor 및 signup http 파일 수정
    UserControllerAdvice 코드 ResponseDTO에 맞게 코드 수정 및
    회원가입시 휴대폰번호를 받도록 수정했기에 수정사항 반영하여 signup.http 파일 수정

--- src/main/java/com/example/yanolja/domain/user/controller/UserController.java
+++ src/main/java/com/example/yanolja/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.yanolja.domain.user.dto.CreateUserResponse;
 import com.example.yanolja.domain.user.entity.User;
 import com.example.yanolja.domain.user.service.UserService;
 import com.example.yanolja.global.springsecurity.PrincipalDetails;
+import com.example.yanolja.global.util.ResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,9 +25,15 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid CreateUserRequest createUserRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(userService.signup(createUserRequest));
+    public ResponseEntity<ResponseDTO<Object>> signup(
+        @RequestBody @Valid CreateUserRequest createUserRequest) {
+        CreateUserResponse response = userService.signup(createUserRequest);
+        ResponseDTO<Object> successResponse = ResponseDTO.res(
+            HttpStatus.CREATED,
+            "회원가입에 성공",
+            response);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     // Authenticated user 샘플테스트 코드입니다
@@ -35,7 +42,7 @@ public class UserController {
         User user = principalDetails.getUser();
 
         CreateUserResponse createUserResponse = new CreateUserResponse(user.getEmail(),
-            user.getUsername());
+            user.getUsername(), user.getPhonenumber());
         return ResponseEntity.ok(createUserResponse);
     }
 }
