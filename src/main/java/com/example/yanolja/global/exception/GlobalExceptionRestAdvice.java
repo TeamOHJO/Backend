1b02906c398e140d715928d696b72d1568f38e52
liyusang1 <liyusang1@naver.com>
2023-11-22 22:20:32 +0900

    [feat] GlobalExceptionRestAdvice에 notnull을 핸들링하도록 코드 추가
    notnull값에 null 할당시 에러가 처리되지 않고 MethodArgumentNotValidException가 발생만 하고 있어서 코드를 추가하여 에러를 핸들링 하여 ResponseDTO에 담아서 응답을 보내도록 코드 수정

--- src/main/java/com/example/yanolja/global/exception/GlobalExceptionRestAdvice.java
+++ src/main/java/com/example/yanolja/global/exception/GlobalExceptionRestAdvice.java
@@ -2,11 +2,16 @@ package com.example.yanolja.global.exception;
 
 
 import com.example.yanolja.global.util.ResponseDTO;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -45,6 +50,24 @@ public class GlobalExceptionRestAdvice {
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ResponseDTO.res(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러!"));
+    }
+
+    //notnull값을 넣지 않은 경우 에러 핸들링
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO<Object>> handleValidationExceptions(
+        MethodArgumentNotValidException e) {
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> fieldErrors = bindingResult.getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ResponseDTO.res(HttpStatus.BAD_REQUEST, fieldErrors));
     }
 }
 
