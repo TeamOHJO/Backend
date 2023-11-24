572a76eddb5e53164047e263a024ef8f8977985f
liyusang1 <liyusang1@naver.com>
2023-11-24 17:46:18 +0900

    [hotfix] entity @Setter 제거
    entity @Setter 제거

--- src/main/java/com/example/yanolja/domain/user/entity/User.java
+++ src/main/java/com/example/yanolja/domain/user/entity/User.java
@@ -10,10 +10,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User extends BaseTimeEntity {
