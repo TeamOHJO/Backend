ce5bd36e6561a395625311dfa3a242cbc668ffa3
deltawing71911 <deltawing71911@gmail.com>
2023-11-26 11:45:44 +0900

    [feat] 리뷰 삭제기능 수정 및 작동 확인
    resolved #14

--- src/main/java/com/example/yanolja/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/yanolja/domain/review/service/ReviewServiceImpl.java
@@ -1,15 +1,23 @@
 package com.example.yanolja.domain.review.service;
 
+import com.example.yanolja.domain.accommodation.entity.AccommodationCategory;
+import com.example.yanolja.domain.accommodation.entity.AccommodationRooms;
 import com.example.yanolja.domain.accommodation.repository.AccommodationRoomRepository;
 import com.example.yanolja.domain.review.dto.AccommodationReviewResponse;
+import com.example.yanolja.domain.review.dto.ReviewCreateRequest;
+import com.example.yanolja.domain.review.dto.ReviewUpdateRequest;
 import com.example.yanolja.domain.review.dto.RoomReviewResponse;
 import com.example.yanolja.domain.review.entity.Review;
+import com.example.yanolja.domain.review.error.ReviewNotFoundException;
 import com.example.yanolja.domain.review.repository.ReviewRepository;
+import com.example.yanolja.domain.user.entity.User;
 import com.example.yanolja.domain.user.repository.UserRepository;
+import com.example.yanolja.global.exception.ErrorCode;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
@@ -27,38 +35,41 @@ public class ReviewServiceImpl implements ReviewService {
         this.roomRepository = roomRepository;
     }
 
-//    @Override
-//    @Transactional
-//    public RoomReviewResponse createReview(Long roomId, ReviewCreateRequest request) {
-//        AccommodationRooms room = roomRepository.findById(roomId)
-//            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.INVALID_ACCOMMODATION_ID));
-//
-//        User user = userRepository.findById(request.getUserId())
-//            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.USER_NOT_FOUND));
-//
-//        Review review = Review.builder()
-//            .room(room)
-//            .user(user)
-//            .reviewContent(request.getReviewContent())
-//            .image(request.getImage())
-//            .star(request.getStar())
-//            .build();
-//        review.setRoom(room);
-//        review.setUser(user);
-//        review.setReviewContent(request.getReviewContent());
-//        review.setImage(request.getImage());
-//        review.setStar(request.getStar());
-//
-//        Review savedReview = reviewRepository.save(review);
-//
-//        return new RoomReviewResponse(
-//            savedReview.getReviewId(),
-//            roomId, user.getUserId(),
-//            savedReview.getReviewContent(),
-//            savedReview.getImage(),
-//            savedReview.getStar(),
-//            savedReview.getUpdatedAt());
-//    }
+    @Override
+    @Transactional
+    public RoomReviewResponse createReview(Long roomId, ReviewCreateRequest request) {
+        AccommodationRooms room = roomRepository.findById(roomId)
+            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.INVALID_ACCOMMODATION_ID));
+
+        User user = userRepository.findById(request.getUserId())
+            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        Review review = Review.builder()
+            .room(room)
+            .user(user)
+            .reviewContent(request.getReviewContent())
+            .image(request.getImage())
+            .star(request.getStar())
+            .build();
+        review.setRoom(room);
+        review.setUser(user);
+        review.setReviewContent(request.getReviewContent());
+        review.setImage(request.getImage());
+        review.setStar(request.getStar());
+
+        Review savedReview = reviewRepository.save(review);
+
+        return new RoomReviewResponse(
+            savedReview.getReviewId(),
+            roomId,
+            user.getUsername(),
+            room.getName(),
+            room.getAccommodation().getCategory(),
+            savedReview.getReviewContent(),
+            savedReview.getImage(),
+            savedReview.getStar(),
+            savedReview.getUpdatedAt());
+    }
 
 
     @Override
@@ -92,38 +103,39 @@ public class ReviewServiceImpl implements ReviewService {
             .collect(Collectors.toList());
     }
 
-//    @Override
-//    @Transactional
-//    public RoomReviewResponse updateReview(Long reviewId, ReviewUpdateRequest request) {
-//        Review review = reviewRepository.findById(reviewId)
-//            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.REVIEW_NOT_FOUND));
-//
-//        review.setReviewContent(request.getReviewContent());
-//        review.setImage(request.getImage());
-//        review.setStar(request.getStar());
-//
-//        return new RoomReviewResponse(
-//            review.getReviewId(),
-//            review.getRoom().getRoomId(),
-//            review.getUser().getUserId(),
-//            review.getReviewContent(),
-//            review.getImage(),
-//            review.getStar(),
-//            review.getUpdatedAt());
-//    }
-//
-//    @Override
-//    @Transactional
-//    public void deleteReview(Long reviewId) {
-//        Review review = reviewRepository.findById(reviewId)
-//            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.REVIEW_NOT_FOUND));
-//
-//        // 리뷰가 활성된 상태가 아닌 경우 예외 발생 (수정 중, 관리자만 볼 경우 등등)
-//        if (!review.isActive()) {
-//            throw new ReviewNotFoundException(ErrorCode.REVIEW_NOT_FOUND);
-//        }
-//
-//        reviewRepository.deleteById(reviewId);
-//    }
+    @Override
+    @Transactional
+    public RoomReviewResponse updateReview(Long reviewId, ReviewUpdateRequest request) {
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.REVIEW_NOT_FOUND));
+
+        review.setReviewContent(request.getReviewContent());
+        review.setImage(request.getImage());
+        review.setStar(request.getStar());
+
+        String username = review.getUser().getUsername();
+        String roomName = review.getRoom().getName();
+        AccommodationCategory category = review.getRoom().getAccommodation().getCategory();
+
+        return new RoomReviewResponse(
+            review.getReviewId(),
+            review.getRoom().getRoomId(),
+            username,
+            roomName,
+            category,
+            review.getReviewContent(),
+            review.getImage(),
+            review.getStar(),
+            review.getUpdatedAt());
+    }
+
+    @Override
+    @Transactional
+    public void deleteReview(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new ReviewNotFoundException(ErrorCode.REVIEW_NOT_FOUND));
+
+        reviewRepository.delete(review);
+    }
 
 }
