2e73b3a8f89826d013f9cd5c0ccabc22156fb52b
liyusang1 <liyusang1@naver.com>
2023-11-27 20:42:02 +0900

    [feat] reviewImages entity 추가
    reviewImages entity 추가

---
+++ src/main/java/com/example/yanolja/domain/review/entity/ReviewImages.java
@@ -0,0 +1,38 @@
+
+package com.example.yanolja.domain.review.entity;
+
+import com.example.yanolja.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ReviewImages extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+
+    @ManyToOne
+    @JoinColumn(name = "reviewId", nullable = false)
+    private Review review;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Builder
+    ReviewImages(Review review, String image) {
+        this.review = review;
+        this.image = image;
+    }
+}
