e30b49303dce0893e0f5eaf33888e5654727cf0c
liyusang1 <liyusang1@naver.com>
2023-11-27 21:14:59 +0900

    [feat] 리뷰 조회 기능 추가 리뷰의 모든 이미지를 가져올 수 있도록 변경
    리뷰 조회 기능 추가 리뷰의 모든 이미지를 가져올 수 있도록 변경

--- src/main/java/com/example/yanolja/domain/review/dto/AccommodationReviewResponse.java
+++ src/main/java/com/example/yanolja/domain/review/dto/AccommodationReviewResponse.java
@@ -2,6 +2,7 @@ package com.example.yanolja.domain.review.dto;
 
 import com.example.yanolja.domain.accommodation.entity.AccommodationCategory;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +16,10 @@ public class AccommodationReviewResponse {
     private Long roomId;
     private String username;
     private String accommodationName;
+    private String roomName;
     private AccommodationCategory category;
     private String reviewContent;
     private int star;
+    private List<String> images;
     private LocalDateTime updatedAt;    // 리뷰 수정 시간
-
 }
