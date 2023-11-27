181e7af1d1b8587f6d13cbb08b8262bb12cdc05e
hym <ghrltjdtprbs@gmail.com>
2023-11-27 10:06:02 +0900

    [feat] 숙소조회 수정 및 방 조회기능 구현
    변경 사항 :
     - 숙소 조회 기능 리팩토링(maxCapacity로 필터링 추가)
     - 방 조회 기능 구현 완료

--- src/main/java/com/example/yanolja/domain/accommodation/dto/AccommodationDetailResponse.java
+++ src/main/java/com/example/yanolja/domain/accommodation/dto/AccommodationDetailResponse.java
@@ -2,17 +2,28 @@ package com.example.yanolja.domain.accommodation.dto;
 
 import com.example.yanolja.domain.accommodation.entity.AccommodationCategory;
 import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class AccommodationDetailResponse {
 
     private Long accommodationId;
     private String name;
     private String location;
-    private String description;
+    private boolean isDomestic;
     private AccommodationCategory category;
-    private boolean isLiked;
     private double averageRating;
+    private String tag;
+    private String description;
+    private String explanation;
+    private String cancelInfo;
+    private String useGuide;
+    private String reservationNotice;
     private List<String> accommodationImages;
+    private List<String> serviceInfo;
     private List<RoomDetail> roomDetails;
+
 
 }
--- src/main/java/com/example/yanolja/domain/accommodation/repository/AccommodationRoomRepository.java
+++ src/main/java/com/example/yanolja/domain/accommodation/repository/AccommodationRoomRepository.java
@@ -1,9 +1,9 @@
 package com.example.yanolja.domain.accommodation.repository;
 
 import com.example.yanolja.domain.accommodation.entity.AccommodationRooms;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccommodationRoomRepository
-    extends JpaRepository<AccommodationRooms, Long> {
-
+public interface AccommodationRoomRepository extends JpaRepository<AccommodationRooms, Long> {
+    List<AccommodationRooms> findAllByAccommodation_AccommodationId(Long accommodationId);
 }
