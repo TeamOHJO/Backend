762e3c0a2ecf32ed250f5f4750f0aebf565eb265
liyusang1 <liyusang1@naver.com>
2023-11-27 20:42:49 +0900

    [feat] review entity 수정 및 ReviewImageRepository 추가
    review entity 수정 및 ReviewImageRepository 추가

---
+++ src/main/java/com/example/yanolja/domain/review/repository/ReviewImageRepository.java
@@ -0,0 +1,9 @@
+package com.example.yanolja.domain.review.repository;
+
+import com.example.yanolja.domain.review.entity.ReviewImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImages, Long> {
+
+
+}
