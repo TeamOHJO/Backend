db266a3251cb85d2443613319a72d717a088a9b1
deltawing71911 <deltawing71911@gmail.com>
2023-12-06 17:59:38 +0900

    [refactor] 변환로직을 Wishlist DTO 내부에 캡슐화 WishlistServiceImpl은 WishlistDTO 클래스의 정적 메서드를 호출하는 방식으로 바꿈.
    resolved by #50

--- src/main/java/com/example/yanolja/domain/wishlist/dto/WishlistDTO.java
+++ src/main/java/com/example/yanolja/domain/wishlist/dto/WishlistDTO.java
@@ -1,7 +1,12 @@
 package com.example.yanolja.domain.wishlist.dto;
 
 import com.example.yanolja.domain.accommodation.entity.Accommodation;
+import com.example.yanolja.domain.accommodation.entity.AccommodationImages;
+import com.example.yanolja.domain.accommodation.repository.AccommodationImageRepository;
+import com.example.yanolja.domain.accommodation.repository.AccommodationRoomRepository;
 import com.example.yanolja.domain.accommodationLikes.entity.AccommodationLikes;
+import java.util.Arrays;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,8 +24,18 @@ public class WishlistDTO {
     private String location;
     private int price;
 
-    public static WishlistDTO from(AccommodationLikes like, String imageUrl, int lowestPrice) {
+    public static WishlistDTO from(AccommodationLikes like, AccommodationImageRepository accommodationImageRepository, AccommodationRoomRepository accommodationRoomRepository) {
         Accommodation accommodation = like.getAccommodation();
+
+        List<AccommodationImages> images = accommodationImageRepository
+            .findByAccommodation_AccommodationId(accommodation.getAccommodationId());
+        String imageUrl = images.stream()
+            .flatMap(img -> Arrays.stream(img.getImage().split(",\\s*")))
+            .findFirst()
+            .orElse(null);
+
+        int lowestPrice = accommodationRoomRepository.selectMinPrice(accommodation.getAccommodationId());
+
         return new WishlistDTO(
             accommodation.getAccommodationId(),
             imageUrl,
--- src/main/java/com/example/yanolja/domain/wishlist/service/WishlistServiceImpl.java
+++ src/main/java/com/example/yanolja/domain/wishlist/service/WishlistServiceImpl.java
@@ -1,7 +1,5 @@
 package com.example.yanolja.domain.wishlist.service;
 
-import com.example.yanolja.domain.accommodation.entity.Accommodation;
-import com.example.yanolja.domain.accommodation.entity.AccommodationImages;
 import com.example.yanolja.domain.accommodation.repository.AccommodationImageRepository;
 import com.example.yanolja.domain.accommodation.repository.AccommodationRoomRepository;
 import com.example.yanolja.domain.accommodationLikes.entity.AccommodationLikes;
@@ -9,7 +7,6 @@ import com.example.yanolja.domain.accommodationLikes.repository.AccommodationLik
 import com.example.yanolja.domain.user.entity.User;
 import com.example.yanolja.domain.user.repository.UserRepository;
 import com.example.yanolja.domain.wishlist.dto.WishlistDTO;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -22,54 +19,17 @@ public class WishlistServiceImpl implements WishlistService {
     private final AccommodationLikesRepository accommodationLikesRepository;
     private final AccommodationImageRepository accommodationImageRepository;
     private final AccommodationRoomRepository accommodationRoomRepository;
-    private final UserRepository userRepository;
 
     @Override
     public List<WishlistDTO> getUserWishlist(User user) {
-        // 사용자가 좋아요 표시한 숙박업체 목록 조회
+
         List<AccommodationLikes> likes = accommodationLikesRepository.findByUserAndIsLike(user,
             true);
 
         return likes.stream()
-            .filter(like -> like.getAccommodation() != null) // 좋아요 표시된 숙박업체가 있는지 확인!
-            .map(like -> {
-                Accommodation accommodation = like.getAccommodation();
-                List<AccommodationImages> images = accommodationImageRepository
-                    .findByAccommodation_AccommodationId(accommodation.getAccommodationId());
-                String imageUrl = images.stream()
-                    .flatMap(img -> Arrays.stream(img.getImage().split(",\\s*")))
-                    .findFirst()
-                    .orElse(null);
-
-                int lowestPrice = accommodationRoomRepository.selectMinPrice(accommodation.getAccommodationId());
-
-                return WishlistDTO.from(like, imageUrl, lowestPrice);
-            })
+            .filter(like -> like.getAccommodation() != null)
+            .map(like -> WishlistDTO.from(like, accommodationImageRepository, accommodationRoomRepository))
             .collect(Collectors.toList());
     }
 
-//    private WishlistDTO convertToDTO(AccommodationLikes like) {
-//        Accommodation accommodation = like.getAccommodation(); // 좋아요 표시된 숙박업체
-//
-//        List<AccommodationImages> images = accommodationImageRepository
-//            .findByAccommodation_AccommodationId(accommodation.getAccommodationId());
-//        // 이미지 URL 문자열을 쉼표로 분리하고 첫 번째 URL 선택
-//        String imageUrl = images.stream()
-//            .flatMap(img -> Arrays.stream(img.getImage().split(",\\s*")))
-//            .findFirst()
-//            .orElse(null);
-//
-//        int lowestPrice = accommodationRoomRepository.selectMinPrice(
-//            accommodation.getAccommodationId());
-//
-//        return new WishlistDTO(
-//            accommodation.getAccommodationId(),
-//            imageUrl,
-//            accommodation.getAccommodationName(),
-//            accommodation.getCategory().getCategory(),
-//            like.getIsLike(),
-//            accommodation.getLocation(),
-//            lowestPrice
-//        );
-//    }
 }
