44991dd71953b4ed76e7961f2b0adec638220351
hym <ghrltjdtprbs@gmail.com>
2023-12-06 12:08:09 +0900

    [refactor] 필요없는 import문 제거
    변경 사항 :
     - UpdateUserRequest의 필요없는 import문 제거

--- src/main/java/com/example/yanolja/domain/user/dto/UpdateUserRequest.java
+++ src/main/java/com/example/yanolja/domain/user/dto/UpdateUserRequest.java
@@ -1,21 +1,20 @@
 package com.example.yanolja.domain.user.dto;
-import com.example.yanolja.domain.user.entity.User;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateUserRequest {
+
     @NotNull
     private String username;
 
     @NotNull
     @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "전화번호 형식이 유효하지 않습니다.")
     private String phonenumber;
-
 
 
 }
