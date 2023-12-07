3a803515c748cb3a8b61b1f4a51ace074ec7cfc4
liyusang1 <liyusang1@naver.com>
2023-12-07 13:05:43 +0900

    [refactor] Oauth2 로그인에 맞게 PrincipalDetails 수정
    Authentication 객체에 들어갈 수 있는 타입
    UserDetails, OAuth2User을 모두 implement한 PrincipalDetails를 생성하여 하나로 처리

--- src/main/java/com/example/yanolja/global/springsecurity/PrincipalDetails.java
+++ src/main/java/com/example/yanolja/global/springsecurity/PrincipalDetails.java
@@ -3,25 +3,44 @@ package com.example.yanolja.global.springsecurity;
 import com.example.yanolja.domain.user.entity.User;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Getter
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Getter
     private User user;
     private String username;
     private String password;
+    private Map<String, Object> attributes;
 
+    //일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    //OAuth 로그인
+    public PrincipalDetails(User user,Map<String,Object> attributes) {
+        this.user = user;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
@@ -61,5 +80,10 @@ public class PrincipalDetails implements UserDetails {
 
     public String getEmail() {
         return user.getEmail();
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
