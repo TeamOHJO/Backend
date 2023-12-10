2e18fb90ea5ba0b39242982a22971813fe3ebe4f
liyusang1 <liyusang1@naver.com>
2023-12-10 21:15:42 +0900

    [refactor] basket관련 service로직 수정
    basket관련 service로직 수정

--- src/main/java/com/example/yanolja/domain/basket/controller/BasketController.java
+++ src/main/java/com/example/yanolja/domain/basket/controller/BasketController.java
@@ -1,11 +1,14 @@
 package com.example.yanolja.domain.basket.controller;
 
 import com.example.yanolja.domain.basket.dto.CreateBasketRequest;
+import com.example.yanolja.domain.basket.dto.CreateBasketResponse;
+import com.example.yanolja.domain.basket.dto.GetBasketResponse;
 import com.example.yanolja.domain.basket.service.BasketService;
 import com.example.yanolja.domain.user.entity.User;
 import com.example.yanolja.global.springsecurity.PrincipalDetails;
 import com.example.yanolja.global.util.ResponseDTO;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,35 +29,35 @@ public class BasketController {
 
     //장바구니 추가
     @PostMapping("/rooms/{roomsId}")
-    public ResponseEntity<ResponseDTO<?>> addBasket(
+    public ResponseEntity<ResponseDTO<CreateBasketResponse>> addBasket(
         @PathVariable long roomsId,
         @AuthenticationPrincipal PrincipalDetails principalDetails,
         @Valid @RequestBody CreateBasketRequest createBasketRequest) {
         User user = principalDetails.getUser();
 
-        ResponseDTO<?> response = basketService.addBasket(
+        ResponseDTO<CreateBasketResponse> response = basketService.addBasket(
             createBasketRequest, user, roomsId);
         return ResponseEntity.status(response.getCode()).body(response);
     }
 
     //장바구니 조회
     @GetMapping("")
-    public ResponseEntity<ResponseDTO<?>> getBasket(
+    public ResponseEntity<ResponseDTO<List<GetBasketResponse>>> getBasket(
         @AuthenticationPrincipal PrincipalDetails principalDetails) {
         User user = principalDetails.getUser();
 
-        ResponseDTO<?> response = basketService.getBasket(user);
+        ResponseDTO<List<GetBasketResponse>> response = basketService.getBasket(user);
         return ResponseEntity.status(response.getCode()).body(response);
     }
 
     //장바구니 삭제
     @DeleteMapping("/{basketId}")
-    public ResponseEntity<ResponseDTO<?>> deleteBasket(
+    public ResponseEntity<ResponseDTO<Void>> deleteBasket(
         @PathVariable long basketId,
         @AuthenticationPrincipal PrincipalDetails principalDetails) {
         User user = principalDetails.getUser();
 
-        ResponseDTO<?> response = basketService.deleteBasket(user, basketId);
+        ResponseDTO<Void> response = basketService.deleteBasket(user, basketId);
         return ResponseEntity.status(response.getCode()).body(response);
     }
 }
