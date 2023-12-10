f60b0c3fb7fe47493f23394b19f1c7eded3447e2
liyusang1 <liyusang1@naver.com>
2023-12-10 22:35:53 +0900

    [refactor] OAuth2LoginSuccessHandler 수정
    OAuth2LoginSuccessHandler 수정

--- src/main/java/com/example/yanolja/global/springsecurity/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/yanolja/global/springsecurity/OAuth2LoginSuccessHandler.java
@@ -1,19 +1,21 @@
 package com.example.yanolja.global.springsecurity;
 
-import com.example.yanolja.domain.user.dto.LoginResponse;
 import com.example.yanolja.global.jwt.JwtProvider;
-import com.example.yanolja.global.util.ResponseDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author liyusang1
+ * @implNote 이것은 OAuth 로그인 성공 후 로직을 처리 하는 클래스 입니다.
+ */
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -23,31 +25,23 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
-        // OAuth2 로그인이 성공했을 때의 추가 작업을 수행
-        // 여기에서는 JWT 토큰을 발급하고 형식에 맞게 return
 
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         String token = jwtProvider.createToken(principalDetails.getUser());
+        String email = principalDetails.getEmail();
+        String name = principalDetails.getUsername();
 
-/*        // 응답 헤더에 JWT 토큰 추가
-        response.addHeader("Authorization", "Bearer " + token);
+        //코드 내로 리디렉트 설정
+        //String redirectUrl = "/user/oauth-success?token="+token;
 
-        // JWT 토큰을 response에 담아서 전송
-        response.setContentType("application/json");
-        response.getWriter().write("{\"token\": \"" + token + "\"}");
-        System.out.println("TOKEN : "+token);*/
-
-        ResponseDTO<Object> loginResponse = ResponseDTO.res(
-            HttpStatus.valueOf(HttpServletResponse.SC_OK),
-            "Login successful",
-            new LoginResponse(principalDetails.getUser().getEmail(),
-                principalDetails.getUser().getUsername(), token));
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writeValueAsString(loginResponse);
-
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(jsonResponse);
+        //한국어 인코딩 설정
+        String encodedName = URLEncoder.encode(name, StandardCharsets.UTF_8.toString());
+        
+        String redirectUrl = "http://localhost:5173/social-loading?token=" + token
+        +"&email="+email+"&name="+encodedName;
+        
+        //프론트 배포사이트
+        //https://dashing-tiramisu-cbdade.netlify.app/
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
