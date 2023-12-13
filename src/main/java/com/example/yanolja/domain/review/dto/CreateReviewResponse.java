021c1227e3ae9bee738467a0b49b6eea85fbf447
hym <ghrltjdtprbs@gmail.com>
2023-12-13 19:12:36 +0900

    [refactor] CreateReviewResponse 검증로직 제거
    문제 사항 :
     - 유효성 검증은 일반적으로 입력 DTO에서 수행.
    변경 사항 :
     - 불필요한 검증로직 모두 제거

--- src/main/java/com/example/yanolja/domain/review/dto/CreateReviewResponse.java
+++ src/main/java/com/example/yanolja/domain/review/dto/CreateReviewResponse.java
@@ -1,29 +1,24 @@
 package com.example.yanolja.domain.review.dto;
 
 import com.example.yanolja.domain.review.entity.Review;
-import com.example.yanolja.domain.review.entity.ReviewImages;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public record CreateReviewResponse(
-    @NotNull
-    @Size(max = 500, message = "리뷰 내용은 500자를 넘으면 안됩니다.")
+
     String reviewContent,
-    @NotNull
+
     List<String> images,
-    @NotNull
+
     int star
 
 ) {
+
     public static CreateReviewResponse fromReview(Review review,
         List<String> images) {
         return new CreateReviewResponse(
             review.getReviewContent(),
-            review.getReviewImages().stream()
-                .map(ReviewImages::getImage)
-                .collect(Collectors.toList()),
+            images,
             review.getStar()
 
         );
