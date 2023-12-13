046a476c83583248470eee9d4405904660c05686
hym <ghrltjdtprbs@gmail.com>
2023-12-13 18:57:29 +0900

    [refactor] REVIEW 등록 리팩토링
    변경 사항 :
     - 리뷰 등록 메서드의 중복로직 분리

--- src/main/java/com/example/yanolja/domain/review/dto/CreateReviewResponse.java
+++ src/main/java/com/example/yanolja/domain/review/dto/CreateReviewResponse.java
@@ -1,9 +1,11 @@
 package com.example.yanolja.domain.review.dto;
 
 import com.example.yanolja.domain.review.entity.Review;
+import com.example.yanolja.domain.review.entity.ReviewImages;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record CreateReviewResponse(
     @NotNull
@@ -15,12 +17,18 @@ public record CreateReviewResponse(
     int star
 
 ) {
-    public static CreateReviewResponse fromEntity(Review review,
+    public static CreateReviewResponse fromReview(Review review,
         List<String> images) {
         return new CreateReviewResponse(
             review.getReviewContent(),
-            images,
+            review.getReviewImages().stream()
+                .map(ReviewImages::getImage)
+                .collect(Collectors.toList()),
             review.getStar()
+
         );
+
+
     }
+
 }
